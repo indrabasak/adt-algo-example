aef671d948be0aa02a6bb0ddac0cc449c60b7abc
indrabasak <indra.basak1@gmail.com>
2021-08-26 10:42:57 -0700

    updated dependency libs and configured to generate jacoco report

---
+++ src/main/java/com/basaki/algodaily/Calculator.java
@@ -0,0 +1,4 @@
+package com.basaki.algodaily;
+
+public class Calculator {
+}
---
+++ src/test/java/com/basaki/codility/algodaily/MinStackTest.java
@@ -0,0 +1,31 @@
+package com.basaki.codility.algodaily;
+
+import com.basaki.algodaily.MinStack;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MinStackTest {
+
+    @Test
+    public void testMin() {
+        MinStack minstack = new MinStack();
+        minstack.push(4);
+        minstack.push(7);
+        minstack.push(3);
+        minstack.push(2);
+        minstack.push(6);
+        int min1 = minstack.min();
+        minstack.pop();
+        int min2 = minstack.min();
+        minstack.pop();
+        int min3 = minstack.min();
+        minstack.pop();
+        int min4 = minstack.min();
+        minstack.pop();
+        assertEquals(min1, 2);
+        assertEquals(min2, 2);
+        assertEquals(min3, 3);
+        assertEquals(min4, 4);
+    }
+}
